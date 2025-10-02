b87daf3ad75cb717df9aae427f21cb72a7a79cd8
Kir4R00t <miki4sa@interia.pl>
2025-10-02 23:15:30 +0200

    updating readme, removing a feature

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # FarmerBot
 Discord bot made 100% in python for official xfarmerx's discord
 
-## Deployment
+## Available commands
+```/poe2scout <currency_type> ``` - Queries poe2scout.com website for recent currency prices<br>
+```/gibcat``` - Gets an image of a sweet kitty <br>
 
-### Windows
+## Setting up virtualenv
+#### Windows
 ```python -m venv .venv```  <br>
 ```.\.venv\Scripts\Activate.ps1```
 
-### Linux
+#### Linux
 ```python3 -m venv .venv```   <br>
 ```source .venv/bin/activate```
 
-### Docker compose build
+## Docker deployment
 ```docker compose up -d --build```
--- bot/bot.py
+++ bot/bot.py
@@ -49,9 +49,6 @@ async def on_ready():
 async def on_message(message):
     if message.content.lower() == 'ping':
         await message.channel.send('pong')
-    
-    if message.content.lower() == 'ding':
-        await message.channel.send('dong')
 
 # CATSSS
 @bot.tree.command(name="gibcat", description="Get a random image of a cat :3")
@@ -91,6 +88,8 @@ async def poe2scout(interaction: discord.Interaction, category: app_commands.Cho
     selected = category.value
     if selected == "currency":
         url = 'https://poe2scout.com/api/items/currency/currency?referenceCurrency=exalted&page=1&perPage=25&league=Rise%20Of%20The%20Abyssal'
+    
+    # Rest needs refractoring
     elif selected == "soul_cores":
         url = 'https://poe2scout.com/api/items/currency/ultimatum'
     elif selected == "breachstones":
