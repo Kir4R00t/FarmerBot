278f38f405cf56fbdc8fbe0c1f7a7ff79aa38526
Kir4R00t <miki4sa@interia.pl>
2025-10-20 14:19:37 +0200

    updating filtering message

--- bot/bot.py
+++ bot/bot.py
@@ -41,6 +41,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     msg_content = message.content.lower()
+    msg_mention = message.author.mention
 
     # Ignore all bot messages
     if message.author.bot:
@@ -56,11 +57,11 @@ async def on_message(message):
         trade_site_url = 'www.pathofexile.com/trade2/'
         if message.attachments and trade_site_url not in msg_content:
             embed=discord.Embed (
-                title="Your message has been deleted !", 
-                description='Please make sure to attach a screenshot of your item and to provide a valid poe2trade side link to make the pricechecking process easier ! ', 
+                title="Your message was deleted!", 
+                description=f'{msg_mention} Please provide a valid trade link + screenshot of your item to get a price check.', 
                 color=0xff0000
                 )
-            embed.set_footer(text="Please check pinned messages")
+            embed.set_footer(text="See pinned message.")
             
             await message.channel.send(embed=embed)
             await message.delete()
