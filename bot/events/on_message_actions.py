c50841a3eb0813e292ba23fd0b71b17e11c27f04
Kir4R00t <miki4sa@interia.pl>
2025-11-17 20:35:40 +0100

    fixed message filtering

--- bot/events/on_message_actions.py
+++ bot/events/on_message_actions.py
@@ -13,9 +13,12 @@ class OnMessageActions():
             msg_mention = message.author.mention
             
             bot_dev_id = 419571860041105410 # My discord UUID
+
             poe2_price_check_channel_id = 1336034291427053578
+            poe2_trade_site_url = 'www.pathofexile.com/trade2/'
+
             poe1_price_check_channel_id = 1382141426103750736
-            trade_site_url = 'www.pathofexile.com/trade2/'
+            poe1_trade_site_url = 'www.pathofexile.com/trade/'
 
             # Ignore all bot messages
             if message.author.bot:
@@ -26,9 +29,22 @@ class OnMessageActions():
                 if msg_content == 'ping':
                     await message.channel.send('pong')
 
-            # Pricecheck channel message check
-            if message.channel.id == poe2_price_check_channel_id or message.channel.id == poe1_price_check_channel_id:
-                if message.attachments and trade_site_url not in msg_content:
+            # POE2 Pricecheck channel message check
+            if message.channel.id == poe2_price_check_channel_id:
+                if message.attachments and poe2_trade_site_url not in msg_content:
+                    embed=discord.Embed (
+                        title="Your message was deleted!", 
+                        description=f'{msg_mention} Please provide a valid trade link + screenshot of your item to get a price check.', 
+                        color=0xff0000
+                        )
+                    embed.set_footer(text="See pinned message.")
+                    
+                    await message.channel.send(embed=embed)
+                    await message.delete()
+
+            # POE1 Pricecheck channel message check
+            if message.channel.id == poe1_price_check_channel_id:
+                if message.attachments and poe1_trade_site_url not in msg_content:
                     embed=discord.Embed (
                         title="Your message was deleted!", 
                         description=f'{msg_mention} Please provide a valid trade link + screenshot of your item to get a price check.', 
